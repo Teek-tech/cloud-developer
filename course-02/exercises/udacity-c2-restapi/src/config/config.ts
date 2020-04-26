7b5074bf568f5af6db13f4b5986ed202d6076d6f
Tkay <tochukwuodoeme@gmail.com>
2020-04-26 16:38:39 +0100

    aws update

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "udagramteek",
+    "password": "tochizy1173",
+    "database": "udagramteek",
+    "host": "udagramteek.cfewf5i6ld8z.us-east-2.rds.amazonaws.com",
     "dialect": "postgres",
     "aws_region": "us-east-2",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "udagramcuket-dev"
   },
   "prod": {
     "username": "",
